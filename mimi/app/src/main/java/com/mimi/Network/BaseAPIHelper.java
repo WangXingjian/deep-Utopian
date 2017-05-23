c710d00c4b304ae83b3202f1c2240cdca3b7cd23
GamblerJJ <xingjian1205@gmail.com>
2017-05-23 20:52:58 +0800

    first push on wxj

--- mimi/app/src/main/java/com/mimi/Network/BaseAPIHelper.java
+++ mimi/app/src/main/java/com/mimi/Network/BaseAPIHelper.java
@@ -6,6 +6,10 @@ import io.reactivex.disposables.Disposable;
  * Created by wangxingjian on 2017/5/23.
  */
 
+/**
+ * Later make comments!
+ */
+
 public interface BaseAPIHelper<T> {
     void doSubscribe(Disposable d);
     void doNext(T t);
